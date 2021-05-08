8a1d25c5e8f7aad4296b91ee3abbd1ab100baf50
Karan Kangude <karankangude17@gmail.com>
2021-05-08 20:43:06 +0530

    ui changes-1

--- WebApplication/src/index.js
+++ WebApplication/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-import App from './App';
+import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
